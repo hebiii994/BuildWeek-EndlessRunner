d211f5e1e2460499078caa841ef155bae4cbea5b
hebiii994 <salvatoreguglielmini964@gmail.com>
2025-09-19 20:47:38 +0200

    final push

--- Assets/_Project/Scripts/UI/SceneNames.cs
+++ Assets/_Project/Scripts/UI/SceneNames.cs
@@ -1,8 +1,6 @@
 public static class SceneNames
 {
-    //public const string MainMenu = "MainMenu";
-    //public const string Game = "GameScene";
+    public const string MainMenu = "MainMenu";
+    public const string Game = "GameScene";
 
-    public const string MainMenu = "MainMenu_salvo";
-    public const string Game = "GameScene2";
 }
