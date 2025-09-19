8fb155d65dd2cf96c543e980222f526e24fc2aae
Alessio Berruto <alessioberrutoit@gmail.com>
2025-09-19 16:43:55 +0800

    aggiunto nome scene mie per testing

--- Assets/_Project/Scripts/UI/SceneNames.cs
+++ Assets/_Project/Scripts/UI/SceneNames.cs
@@ -1,5 +1,8 @@
 public static class SceneNames
 {
-    public const string MainMenu = "MainMenu";
-    public const string Game = "GameScene";
+    //public const string MainMenu = "MainMenu";
+    //public const string Game = "GameScene";
+
+    public const string MainMenu = "MainMenu_salvo";
+    public const string Game = "GameScene2";
 }
